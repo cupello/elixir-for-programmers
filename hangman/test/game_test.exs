670570ae617f9777dfca65cccbb2c4356296e25f
Rafael Cupello <rafacupello@gmail.com>
2018-02-07 09:22:42 -0200

    Reduce Session

--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -67,26 +67,20 @@ defmodule GameTest do
   end
 
   test "lost game is recognized" do
-    game = Game.new_game("w")
-    {game, _tally} = Game.make_move(game, "a")
-    assert game.game_state == :bad_guess
-    assert game.turns_left == 6
-    {game, _tally} = Game.make_move(game, "b")
-    assert game.game_state == :bad_guess
-    assert game.turns_left == 5
-    {game, _tally} = Game.make_move(game, "c")
-    assert game.game_state == :bad_guess
-    assert game.turns_left == 4
-    {game, _tally} = Game.make_move(game, "d")
-    assert game.game_state == :bad_guess
-    assert game.turns_left == 3
-    {game, _tally} = Game.make_move(game, "e")
-    assert game.game_state == :bad_guess
-    assert game.turns_left == 2
-    {game, _tally} = Game.make_move(game, "f")
-    assert game.game_state == :bad_guess
-    assert game.turns_left == 1
-    {game, _tally} = Game.make_move(game, "g")
-    assert game.game_state == :lost
+    moves = [
+      {"w", :good_guess},
+      {"i", :good_guess},
+      {"b", :good_guess},
+      {"l", :good_guess},
+      {"e", :won}
+    ]
+
+    game = Game.new_game("wibble")
+
+    Enum.reduce(moves, game, fn {guess, state}, game ->
+      {game, _tally} = Game.make_move(game, guess)
+      assert game.game_state == state
+      game
+    end)
   end
 end
