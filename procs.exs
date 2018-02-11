78b7d2d9e631930b681a741893071568436be3fc
Rafael Cupello <rafacupello@gmail.com>
2018-02-11 14:56:42 -0200

    Pattern Matching Messages

--- procs.exs
+++ procs.exs
@@ -30,4 +30,37 @@ defmodule Procs do
 
     greeter(what_to_say)
   end
+
+  @doc """
+  iex(2)> spawn Procs, :greeter_with_counter , [0]
+  #PID<0.92.0>
+  iex(3)> pid = v(-1)
+  #PID<0.92.0>
+  iex(5)> send pid, "rafa"
+  0: Hello "rafa"
+  "rafa"
+  iex(6)> send pid, {:add , 2}
+  {:add, 2}
+  iex(7)> send pid, "rafa"
+  2: Hello "rafa"
+  "rafa"
+  iex(20)> send pid, {:reset}
+  {:reset}
+  iex(21)> send pid, "rafa"
+  0: Hello "rafa"
+  "rafa"
+  """
+  def greeter_with_counter(count) do
+    receive do
+      {:reset} ->
+        greeter_with_counter(0)
+
+      {:add, n} ->
+        greeter_with_counter(count + n)
+
+      msg ->
+        IO.puts("#{count}: Hello #{inspect(msg)}")
+        greeter_with_counter(count)
+    end
+  end
 end
