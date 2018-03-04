40acbdafe6ece9bb8e0189ae5ab52bdf654c75fc
Rafael Cupello <rafacupello@gmail.com>
2018-03-04 11:10:28 -0300

    Pushing the Tally from the Server

--- socket_gallows/lib/socket_gallows_web/channels/hangman_channel.ex
+++ socket_gallows/lib/socket_gallows_web/channels/hangman_channel.ex
@@ -2,6 +2,17 @@ defmodule SocketGallowsWeb.HangmanChannel do
   use Phoenix.Channel
 
   def join("hangman:game", _, socket) do
+    game = Hangman.new_game()
+    socket = assign(socket, :game, game)
     {:ok, socket}
+  end
+
+  def handle_in("tally", _, socket) do
+    tally =
+      socket.assigns.game
+      |> Hangman.tally()
+
+    push(socket, "tally", tally)
+    {:noreply, socket}
   end
 end
